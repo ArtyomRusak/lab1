e0c0823186b271429670bcf9c87e5e45b2d2410d
ArtyomRusak <bembi1204@gmail.com>
2013-11-06 00:44:12 +0300

    [Added]: Start to implement Matrix. [ArtyomRusak]

---
+++ MatrixExercise/MatrixExercise/Matrix.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixExercise
+{
+    public abstract class Matrix<T>
+    {
+        public abstract void Add(int i, int j, T element);
+        public abstract void Edit(int i, int j, T element);
+        public abstract T GetElement(int i, int j);
+        public abstract void ShowMatrix();
+        public abstract T this[int i, int j] { get; set; }
+    }
+}
